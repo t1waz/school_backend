fb49fb90c7686a097eb9a3c11eccfc0453922f38
Julian Korgol <kontakt@juliankorgol.com>
2022-09-30 16:40:56 +0200

    Julian example endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from datetime import datetime
 
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
+async def julian_endpoint(request):
+    date = str(datetime.now())
+    return JSONResponse({'date': date})
+
 
 app = Starlette(debug=True, routes=[
     Route('/hello', homepage),
+    Route('/julian', julian_endpoint),
 ])
