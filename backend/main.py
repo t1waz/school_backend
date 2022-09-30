65f3637cf5a5f29072b31c9a4cc840356cc72db9
Ghost6818 <nataliaostanska2000@gmail.com>
2022-09-30 16:37:56 +0200

    school

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ from starlette.routing import Route
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
+async def school(request):
+    return JSONResponse({'lesson': 'software engineering'})
+
 
 app = Starlette(debug=True, routes=[
     Route('/hello', homepage),
+    Route('/lesson', school),
 ])
